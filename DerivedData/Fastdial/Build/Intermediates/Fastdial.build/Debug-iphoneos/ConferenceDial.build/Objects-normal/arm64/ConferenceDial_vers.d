99d396aeffd6837ca4e425d20695a2117827c26b
Brian J Hernacki <brian.j.hernacki@intel.com>
2017-04-06 11:09:54 -0700

    removing derived data files from repo

--- DerivedData/Fastdial/Build/Intermediates/Fastdial.build/Debug-iphoneos/ConferenceDial.build/Objects-normal/arm64/ConferenceDial_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/bjhernac/Dropbox/src/fastdial/DerivedData/Fastdial/Build/Intermediates/Fastdial.build/Debug-iphoneos/ConferenceDial.build/DerivedSources/ConferenceDial_vers.c
--- DerivedData/Fastdial/Build/Intermediates/Fastdial.build/Debug-iphonesimulator/ConferenceDial.build/Objects-normal/x86_64/ConferenceDial_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/bjhernac/Dropbox/src/fastdial/DerivedData/Fastdial/Build/Intermediates/Fastdial.build/Debug-iphonesimulator/ConferenceDial.build/DerivedSources/ConferenceDial_vers.c
